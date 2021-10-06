f60e8fabf8cb5d506b0080a533920945274a600e
Davi_Tolen <davitolen@outlook.com>
2021-10-05 21:07:39 -0300

    teste

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+function App() {
+  const frase = "Hello World!!";
+  const numero = 40;
+
+  function clicou(exibicao){
+      alert(exibicao * 10)
+  }
+
+  return (
+    <div>
+      <h1>{frase + ":)"}{numero + 1}</h1>
+      <button onClick={() => clicou(20)}
+
+    >
+      20
+      </button>
+
+      <button onClick={() => clicou(100)}
+
+    >
+      100
+      </button>
+    </div>
+  );
+}
+
+export default App;
