4035ef77fd2fbb5428ac5eaa0be2c820e98312af
Davi_Tolen <davitolen@outlook.com>
2021-10-07 10:38:56 -0300

    Apagando os arquivos da aula de props

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import React from "react";
-import Membro from "./Membro";
-import Title from "./Title";
 
-const braga = {
-  nome: "Arthur Braga",
-  idade: 19,
-  cargo: "Gerente de Produtos",
-  curso: "Engenharia de Sistemas",
-}
-
-const lima = {
-  nome: "Arthur Lima",
-  idade: 19,
-  cargo: "Gerente Scrum",
-  curso: "Engenharia Elétrica",
-}
 
 function App() {
   return (
     <div>
-      <Title title="Membros">
-      <Membro membro={braga}/>
-      <Membro membro={lima}/>
-      </Title>
+
     </div>
   );
 }
